3ee768de6b613b77da311f2d701807f694036af1
Chmouel Boudjnah <chmouel@chmouel.com>
2016-10-14 14:26:30 +0200

    Generate HTML

---
+++ generate_html.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"text/template"
+)
+
+type WorkoutProgram struct {
+	Repetition  string
+	Meters      string
+	Percentage  string
+	TrackLaps   string
+	TrackLength int
+	Repos       string
+	VMAS        map[string]WorkoutVMA
+}
+
+type WorkoutVMA struct {
+	VMA       string
+	TotalTime string
+	TimeTrack string
+	Speed     string
+	Pace      string
+}
+
+func generate_content(w WorkoutProgram, content *bytes.Buffer) (err error) {
+	t, err := template.ParseFiles("templates/content.tmpl")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = t.Execute(content, w)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func generate_template(content string) (err error) {
+	dico := map[string]string{
+		"Content": content,
+	}
+
+	t, err := template.ParseFiles("templates/template.tmpl")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = t.Execute(os.Stdout, dico)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func generate_html(rounds [][]string) error {
+	var content bytes.Buffer
+
+	for i := range rounds {
+		w := WorkoutProgram{}
+		w.TrackLength = TRACK_LENGTH
+
+		repetition, err := strconv.ParseFloat(rounds[i][0], 64)
+		if err != nil {
+			return err
+		}
+		w.Repetition = fmt.Sprintf("%.f", repetition)
+
+		percentage, err := strconv.ParseFloat(rounds[i][1], 64)
+		if err != nil {
+			return err
+		}
+		w.Percentage = fmt.Sprintf("%.f", percentage)
+		meters, err := strconv.ParseFloat(rounds[i][2], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		w.Meters = fmt.Sprintf("%.f", meters)
+
+		w.Repos = rounds[i][3]
+
+		track_length, err := strconv.ParseFloat(strconv.Itoa(TRACK_LENGTH), 64)
+		if err != nil {
+			return err
+		}
+		track_laps := meters / track_length
+		laps := fmt.Sprintf("%.1f", track_laps)
+		if strings.HasSuffix(laps, ".0") {
+			laps = strings.TrimSuffix(laps, ".0")
+		} else if laps == "0.5" {
+			laps = "½"
+		} else if strings.HasSuffix(laps, ".5") {
+			laps = strings.Replace(laps, ".5", "½", -1)
+		}
+		w.TrackLaps = laps
+
+		vmas := map[string]WorkoutVMA{}
+		for _, vmad := range VMA {
+			wt := WorkoutVMA{}
+			vma := float64(vmad)
+			total_time, err := calcul_vma_distance(vma, percentage, meters)
+			if err != nil {
+				return err
+			}
+			wt.VMA = fmt.Sprintf("%.f", vma)
+			wt.TotalTime = total_time
+			if int(meters) >= TRACK_LENGTH {
+				time_lap, err := calcul_vma_distance(vma, percentage, float64(TRACK_LENGTH))
+				if err != nil {
+					return err
+				}
+				wt.TimeTrack = time_lap
+			} else {
+				wt.TimeTrack = "NA"
+			}
+			speed := calcul_vma_speed(vma, percentage)
+			wt.Speed = fmt.Sprintf("%.2f", speed)
+			wt.Pace = calcul_pace(speed)
+
+			vmas[wt.VMA] = wt
+		}
+		w.VMAS = vmas
+		err = generate_content(w, &content)
+		if err != nil {
+			return err
+		}
+		//spew.Dump(w)
+	}
+	err := generate_template(content.String())
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,10 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"strconv"
-)
+import "log"
 
 var (
 	TRACK_LENGTH = 400
@@ -12,89 +8,23 @@ var (
 
 var VMA = []int{16, 17, 18}
 
-func print_separator(s string) {
-	for i := 0; i < len(s); i++ {
-		fmt.Print("-")
-	}
-	fmt.Println("")
-}
-
-// print_repeat ...
-func print_text_repeat(repetion, percentage, track_laps, meters float64, repos string) {
-	header := fmt.Sprintf("%d * %dm at %d%% / Total laps %d\n",
-		int(repetion), int(meters), int(percentage), int(track_laps))
-
-	print_separator(header)
-	fmt.Print(header)
-	print_separator(header)
-
-	for _, vmad := range VMA {
-		vma := float64(vmad)
-		total_time, err := calcul_vma_distance(vma, percentage, meters)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("VMA: %0.f => Total: %s", vma, total_time)
-		if int(meters) >= TRACK_LENGTH {
-			time_track, err := calcul_vma_distance(vma, percentage, float64(TRACK_LENGTH))
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Printf(" / %d: %s", TRACK_LENGTH, time_track)
-		}
-
-		speed := calcul_vma_speed(vma, percentage)
-		fmt.Printf(" / Speed: %.1fkh / Pace: %s\n", speed, calcul_pace(speed))
-	}
-	fmt.Printf("\nRepos: %s\n", repos)
-	fmt.Println("")
-}
-
-func print_text(rounds [][]string) error {
-	for i := range rounds {
-		repetition, err := strconv.ParseFloat(rounds[i][0], 64)
-		if err != nil {
-			return err
-		}
-
-		percentage, err := strconv.ParseFloat(rounds[i][1], 64)
-		if err != nil {
-			return err
-		}
-
-		meters, err := strconv.ParseFloat(rounds[i][2], 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		track_length, err := strconv.ParseFloat(strconv.Itoa(TRACK_LENGTH), 64)
-		if err != nil {
-			return err
-		}
-		track_laps := meters / track_length
-
-		repos := rounds[i][3]
-
-		print_text_repeat(repetition, percentage, track_laps, meters, repos)
-	}
-	return nil
-}
-
-func print_html(repetion, percentage, track_laps, meters float64, repos string) {
-}
-
 func main() {
 	// converting to float
 
 	var rounds = [][]string{
-		[]string{"1", "90", "1000", "1mn de repos"},
-		[]string{"1", "95", "800", "3mn de repos"},
-		[]string{"1", "100", "600", "3mn de repos"},
-		[]string{"1", "110", "400", "3mn de repos"},
-		[]string{"1", "120", "200", "3mn de repos"},
+		[]string{"1", "90", "1000", "1mn"},
+		[]string{"1", "95", "800", "200m active"},
+		[]string{"1", "100", "600", "3mn"},
+		[]string{"1", "110", "400", "3mn"},
+		[]string{"1", "120", "200", "3mn"},
 	}
-	err := print_text(rounds)
+
+	err := generate_html(rounds)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
+
+// Local Variables:
+// compile-command: "go run main.go generate_html.go calcul.go";
+// End:
