0db2e662f8e346e88faffe7cdbcdccf7bca09a6b
Chmouel Boudjnah <chmouel@chmouel.com>
2016-11-16 12:29:35 +0100

    Adjust banneer, copyright

--- main.go
+++ main.go
@@ -6,18 +6,24 @@ var (
 	TRACK_LENGTH = 400
 )
 
-var VMA = []int{16, 17, 18}
+var VMA = []int{13, 14, 15, 16, 17, 18, 19}
 
 func main() {
 	// converting to float
 
 	var rounds = [][]string{
-		[]string{"1", "90", "1000", "1mn"},
+		[]string{"1", "90", "1000", "200m active"},
 		[]string{"1", "95", "800", "200m active"},
-		[]string{"1", "100", "600", "3mn"},
-		[]string{"1", "110", "400", "3mn"},
-		[]string{"1", "120", "200", "3mn"},
+		[]string{"1", "100", "600", "2mn"},
+		[]string{"1", "110", "400", "2mn"},
+		[]string{"1", "120", "200", "2mn"},
 	}
+
+	// rounds = [][]string{
+	// 	[]string{"8", "110", "100", "1mn30"},
+	// 	[]string{"6", "100", "200", "1mn30"},
+	// 	[]string{"4", "90", "400", "1mn30"},
+	// }
 
 	err := generate_html(rounds)
 	if err != nil {
