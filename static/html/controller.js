76f9bcad7d86028c11aed8e42270ba97f6245505
Chmouel Boudjnah <chmouel@chmouel.com>
2016-12-10 19:35:24 +0100

    Move things around and make routable argument

--- static/html/controller.js
+++ static/html/controller.js
@@ -1,26 +1,21 @@
 var app = angular.module("Frack", ["ngRoute"]);
 
 app.config(function($routeProvider) {
-    $routeProvider.when("/:name", {controller: "FrackController"});
+    $routeProvider.when("/:name", {controller: "FrackController", templateUrl: "frack.html"});
 });
 
-function FrackController($scope, $routeParams, $http) {
-    var workout_name = 'HelloMoto';
-    var vma = '17'
-    $scope.programs = {'ProgramName': "", "Workout": []}
-    console.debug($routeParams);
 
-    res = $http.get('/rest/program/' + workout_name + "/" + vma + "/full");
-	res.success(function(data, status, headers, config) {
-		$scope.programs = data;
-	});
-}
 app.controller("FrackController", ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
     var workout_name = 'HelloMoto';
     var vma = '17'
     $scope.programs = {'ProgramName': "", "Workout": []}
-    console.debug($routeParams.name);
-    return;
+
+    if ($routeParams.name) {
+         workout_name = $routeParams.name
+    } else {
+        //TODO: list all workout to choose from
+        return;
+    }
 
     res = $http.get('/rest/program/' + workout_name + "/" + vma + "/full");
 	res.success(function(data, status, headers, config) {
