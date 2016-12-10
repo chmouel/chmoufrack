1cddb7319bcf1669404ce25586ecfa3f4d2a666e
Chmouel Boudjnah <chmouel@chmouel.com>
2016-12-10 21:15:14 +0100

    Show vma
    Change path to get proper things

--- static/html/controller.js
+++ static/html/controller.js
@@ -1,7 +1,9 @@
 var app = angular.module("Frack", ["ngRoute"]);
 
 app.config(function($routeProvider) {
-    $routeProvider.when("/:name", {controller: "FrackController", templateUrl: "frack.html"});
+    $routeProvider
+        .when("/workout/:name", {controller: "FrackController", templateUrl: "frack.html"})
+        .when("/workout/:name/vma/:vma", {controller: "FrackController", templateUrl: "frack.html"});
 });
 
 
@@ -11,10 +13,14 @@ app.controller("FrackController", ['$scope', '$routeParams', '$http', function($
     $scope.programs = {'ProgramName': "", "Workout": []}
 
     if ($routeParams.name) {
-         workout_name = $routeParams.name
+        workout_name = $routeParams.name;
     } else {
         //TODO: list all workout to choose from
         return;
+    }
+
+    if ($routeParams.vma) {
+        vma = $routeParams.vma;
     }
 
     res = $http.get('/rest/program/' + workout_name + "/" + vma + "/full");
