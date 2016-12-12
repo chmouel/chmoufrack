ddd1414c20650a6e801c66ee87569a245d3708d5
Chmouel Boudjnah <chmouel@chmouel.com>
2016-12-12 17:00:29 +0100

    show index
    and submit when change

--- static/html/show/controller.js
+++ static/html/show/controller.js
@@ -1,27 +1,27 @@
 var app = angular.module("Frack", ["ngRoute"]);
-var vma = '14:19'
+var default_vma = '14:19'
 
 app.config(function($routeProvider) {
     $routeProvider
         .when("/workout/:name", {controller: "FrackController", templateUrl: "partials/frack.html"})
-        .when("/workout/:name/vma/:vma", {controller: "FrackController", templateUrl: "partials/frack.html"});
+        .when("/workout/:name/vma/:vma", {controller: "FrackController", templateUrl: "partials/frack.html"})
+        .otherwise({controller: "FrackController", templateUrl: "partials/frack.html"});
 });
 
 
 app.controller("FrackController", ['$scope', '$location', '$routeParams', '$http', function($scope, $location, $routeParams, $http) {
-    var workout_name = 'HelloMoto';
-    $scope.programs = {'ProgramName': "", "Workout": []}
+    var workout_name = '';
+    $scope.workoutdetail = {'ProgramName': "", "Workout": []}
     $scope.selectedVMA = '';
+    vma = default_vma;
 
     if ($routeParams.name) {
         workout_name = $routeParams.name;
-    } else {
-        //TODO: list all workout to choose from
-        return;
     }
 
     if ($routeParams.vma) {
         vma = $routeParams.vma;
+        $scope.selectedVMA = $routeParams.vma;
     }
 
     res = $http.get('/rest/program/' + workout_name + "/" + vma + "/full");
@@ -29,18 +29,21 @@ app.controller("FrackController", ['$scope', '$location', '$routeParams', '$http
 		$scope.workoutdetail = data;
         $scope.selectedVMA = $scope.workoutdetail.TargetVMA;
 	});
+
     res = $http.get('/rest/programs');
 	res.success(function(data, status, headers, config) {
 		$scope.programs = data;
-        $scope.programs.forEach(function (program, i) {
-            if (program.Name == $scope.workoutdetail.ProgramName) {
-                $scope.selectedWorkout = $scope.programs[i];
-            }
-        });
+
+        if ($scope.workoutdetail.ProgramName != '') {
+            $scope.programs.forEach(function (program, i) {
+                if (program.Name == $scope.workoutdetail.ProgramName) {
+                    $scope.selectedWorkout = $scope.programs[i];
+                }
+            });
+        }
 	});
 
     $scope.submit = function() {
-        console.debug($scope.selectedWorkout.Name);
         $location.path("/workout/" + $scope.selectedWorkout.Name + "/vma/" + $scope.selectedVMA)
     }
 
