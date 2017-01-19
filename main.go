195b1e3e6b66b38d44a5e2917021c7603368ba98
Chmouel Boudjnah <chmouel@chmouel.com>
2017-01-19 18:59:51 +0100

    Add yaml file parsing option

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/chmouel/chmoufrack/server"
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/chmouel/chmoufrack/common"
+	"github.com/chmouel/chmoufrack/server"
+)
 
 func main() {
+	yamlFile := flag.String("y", common.Yaml_File, "Location for the yaml file")
+	flag.Parse()
+
+	common.Yaml_File = *yamlFile
+	if _, err := os.Stat(*yamlFile); os.IsNotExist(err) {
+		log.Fatal("Cannot found: " + *yamlFile)
+	}
 	server.Serve()
 }
