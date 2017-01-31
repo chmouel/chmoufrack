84b08ca6352c1847eff8ec155cb5cf38383493d0
Chmouel Boudjnah <chmouel@chmouel.com>
2017-01-31 09:39:10 +0100

    Move config stuff to its own

---
+++ config.js
@@ -0,0 +1,29 @@
+var app = angular.module("Frack", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/workout/:name", {controller: "FrackController", templateUrl: "partials/frack.html"})
+        .when("/workout/:name/vma/:vma", {controller: "FrackController", templateUrl: "partials/frack.html"})
+        .otherwise({controller: "FrackController", templateUrl: "partials/frack.html"});
+});
+
+
+function range(min, max) {
+    var input = [];
+    min = parseInt(min);
+    max = parseInt(max);
+    for (var i=min; i<=max; i++)
+        input.push(i.toString());
+    return input;
+};
+
+// Disable caching: https://goo.gl/yHW1vE
+app.config(['$httpProvider', function($httpProvider) {
+    //initialize get if not there
+    if (!$httpProvider.defaults.headers.get)
+        $httpProvider.defaults.headers.get = {};
+
+    $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+}]);
--- controller.js
+++ controller.js
@@ -1,34 +1,3 @@
-var app = angular.module("Frack", ["ngRoute"]);
-
-app.config(function($routeProvider) {
-    $routeProvider
-        .when("/workout/:name", {controller: "FrackController", templateUrl: "partials/frack.html"})
-        .when("/workout/:name/vma/:vma", {controller: "FrackController", templateUrl: "partials/frack.html"})
-        .otherwise({controller: "FrackController", templateUrl: "partials/frack.html"});
-});
-
-
-function range(min, max) {
-    var input = [];
-    min = parseInt(min);
-    max = parseInt(max);
-    for (var i=min; i<=max; i++)
-        input.push(i.toString());
-    return input;
-};
-
-// Disable caching: https://goo.gl/yHW1vE
-app.config(['$httpProvider', function($httpProvider) {
-    //initialize get if not there
-    if (!$httpProvider.defaults.headers.get)
-        $httpProvider.defaults.headers.get = {};
-
-    $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
-    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
-    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
-}]);
-
-
 app.controller("FrackController", ['$scope', '$location', '$routeParams', '$http', function($scope, $location, $routeParams, $http) {
     $scope.programWanted = '';
     $scope.vmaWanted = [];
