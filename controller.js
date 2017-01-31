1e0e5335642c6509da0da682264c7178db7ba523
Chmouel Boudjnah <chmouel@chmouel.com>
2017-01-31 09:33:18 +0100

    Disable caching
    We don't care about it anyway, it so small and only happen on reload

--- controller.js
+++ controller.js
@@ -17,10 +17,17 @@ function range(min, max) {
     return input;
 };
 
+// Disable caching: https://goo.gl/yHW1vE
+app.config(['$httpProvider', function($httpProvider) {
+    //initialize get if not there
+    if (!$httpProvider.defaults.headers.get)
+        $httpProvider.defaults.headers.get = {};
 
-app.filter('range', function() {
-    return range;
-});
+    $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+}]);
+
 
 app.controller("FrackController", ['$scope', '$location', '$routeParams', '$http', function($scope, $location, $routeParams, $http) {
     $scope.programWanted = '';
