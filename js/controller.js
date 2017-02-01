aa71fd9cab49fdf77a52d69e05560b296b79d1fe
Chmouel Boudjnah <chmouel@chmouel.com>
2017-02-01 08:59:47 +0100

    Streamline

--- js/controller.js
+++ js/controller.js
@@ -52,23 +52,3 @@ app.controller("FrackController", ['$scope', '$location', '$routeParams', '$http
     };
 
 }]);
-
-app.controller("TabsCtrl", ['$scope', function($scope) {
-    $scope.tabs = [
-        { link : '#!/home', label : 'Home' },
-        { link : '#!/help', label : 'Comment calculer sa VMA' },
-    ];
-
-    $scope.selectedTab = $scope.tabs[0];
-    $scope.setSelectedTab = function(tab) {
-        $scope.selectedTab = tab;
-    };
-
-    $scope.tabClass = function(tab) {
-        if ($scope.selectedTab == tab) {
-            return "active";
-        } else {
-            return "";
-        }
-    };
-}]);
