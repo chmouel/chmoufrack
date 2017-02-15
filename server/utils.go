b5a134a11f23c262628cc20a4a279e7f17eef3d4
Chmouel Boudjnah <chmouel@chmouel.com>
2017-02-15 10:57:35 +0100

    more debug utils

--- server/utils.go
+++ server/utils.go
@@ -1,11 +1,35 @@
 package server
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/davecgh/go-spew/spew"
 )
+
+func DebugVargs(args ...interface{}) {
+	fmt.Printf("(")
+	cnt := 1
+	for _, v := range args {
+		if v == nil {
+			fmt.Printf("'', ")
+			continue
+		}
+
+		switch value := v.(type) {
+		case string:
+			fmt.Printf(`"` + value + `", `)
+		case int:
+			fmt.Printf("%d, ", value)
+		}
+		if cnt != len(args) {
+			fmt.Printf(", ")
+		}
+		cnt += 1
+	}
+	fmt.Println(")")
+}
 
 func Debug(a ...interface{}) {
 	f, err := os.Create("/tmp/debug")
