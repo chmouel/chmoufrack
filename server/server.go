c37bd28d1e860aadf4f21d19615488958374ec66
Chmouel Boudjnah <chmouel@chmouel.com>
2017-02-27 15:56:08 +0100

    Use gorilla handlers for http logging

--- server/server.go
+++ server/server.go
@@ -4,35 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
+	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
-func logger(inner http.Handler, name string) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		inner.ServeHTTP(w, r)
-
-		log.Printf(
-			"%s\t%s\t%s\t%s",
-			r.Method,
-			r.RequestURI,
-			name,
-			time.Since(start),
-		)
-	})
-}
-
-func router(staticDir string) *mux.Router {
+func router(staticDir string) http.Handler {
 	router := mux.NewRouter().StrictSlash(true)
-
 	for _, route := range allRoutes {
 		var handler http.Handler
 		handler = route.HandlerFunc
-		handler = logger(handler, route.Name)
-
 		router.Methods(route.Method).Path(route.Pattern).
 			Name(route.Name).Handler(handler)
 	}
@@ -40,12 +22,13 @@ func router(staticDir string) *mux.Router {
 	s := http.StripPrefix("/", http.FileServer(http.Dir(staticDir)))
 	router.PathPrefix("/").Handler(s)
 
-	return router
+	logRouter := handlers.LoggingHandler(os.Stdout, router)
+	return logRouter
 }
 
 func Serve(staticDir string, port int) {
 	router := router(staticDir)
-	// TODO(chmou): setting
+	sPort := fmt.Sprintf(":%d", port)
 	fmt.Printf("Serving on port %d with static dir %s\n", port, staticDir)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), router))
+	log.Fatal(http.ListenAndServe(sPort, router))
 }
