2a7d62e1be517cf10e493d4aa763f4e45bb245fd
Chmouel Boudjnah <chmouel@chmouel.com>
2017-03-19 12:18:48 +0100

    remove http caching thing

--- client/js/config.js
+++ client/js/config.js
@@ -31,18 +31,6 @@ app.config(function($routeProvider) {
     });
 });
 
-// Disable caching: https://goo.gl/yHW1vE
-app.config(['$httpProvider', function($httpProvider) {
-  //initialize get if not there
-  if (!$httpProvider.defaults.headers.get) {
-    $httpProvider.defaults.headers.get = {};
-  }
-
-  $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
-  $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
-  $httpProvider.defaults.headers.get.Pragma = 'no-cache';
-}]);
-
 //http://stackoverflow.com/a/36254259
 app.directive('input', [function() {
   return {
